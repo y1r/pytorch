8fded05fe91b0fb4ba527678f84b3f898b295251
Yuichiro Ueno <y1r.ueno@gmail.com>
2020-08-06 11:18:35 +0900

    Add non_blocking=True case for storage copy_

--- aten/src/THC/generic/THCTensorCopy.h
+++ aten/src/THC/generic/THCTensorCopy.h
@@ -3,9 +3,7 @@
 #else
 
 THC_API void THCTensor_(copy)(THCState *state, THCTensor *self, THCTensor *src);
+THC_API void THCTensor_(copyAsync)(THCState *state, THCTensor *self, THCTensor *src);
 THC_API void THCTensor_(copyIgnoringOverlaps)(THCState *state, THCTensor *self, THCTensor *src);
-
-THC_API void THCTensor_(copyAsyncCPU)(THCState *state, THCTensor *self, THTensor *src);
-THC_API void THTensor_(copyAsyncCuda)(THCState *state, THTensor *self, THCTensor *src);
 
 #endif
